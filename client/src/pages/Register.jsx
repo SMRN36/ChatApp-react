629a026a647cb3784bb0cd475924c346841e7849
simran bhandari <simranbhandari036@gmail.com>
2023-05-18 18:06:19 +0530

    form validation for registration

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import { useNavigate, Link } from "react-router-dom";
 import Logo from "../assets/logo1.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
-
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
     const [values, setValues] = useState({
         username: "",
         email: "",
@@ -12,15 +20,43 @@ const Register = () => {
         confirmPassword: "",
       });
     
-      
-      
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert("form");
-    }
+    const handleValidation = () => {
+      const { password, confirmPassword, username, email } = values;
+      if (password !== confirmPassword) {
+        toast.error(
+          "Password and confirm password should be same.",
+          toastOptions
+        );
+        return false;
+      } else if (username.length < 3) {
+        toast.error(
+          "Username should be greater than 3 characters.",
+          toastOptions
+        );
+        return false;
+      } else if (password.length < 8) {
+        toast.error(
+          "Password should be equal or greater than 8 characters.",
+          toastOptions
+        );
+        return false;
+      } else if (email === "") {
+        toast.error("Email is required.", toastOptions);
+        return false;
+      }
+  
+      return true;
+    };
+
     const handleChange = (event) => {
         setValues({ ...values, [event.target.name]: event.target.value });
       };
+
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        handleValidation();
+      };
+
   return (
     <>
     <FormContainer>
@@ -39,6 +75,7 @@ const Register = () => {
           </span>
         </form>
     </FormContainer>
+    <ToastContainer />
     </>
   )
 }
