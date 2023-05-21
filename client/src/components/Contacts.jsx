6388188ae1735afd5e292205c36da4e89bb877fd
simran bhandari <simranbhandari036@gmail.com>
2023-05-21 14:44:59 +0530

    Getting all the contacts from database

---
+++ client/src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Logo from "../assets/logo1.svg";
+
+const Contacts = ({contacts}) => {
+  return (
+    <div>Contacts</div>
+  )
+}
+
+export default Contacts
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import Contacts from "../components/Contacts";
 
 const Chat = () => {
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(async () => {
+    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/login");
+    } else {
+      setCurrentUser(
+        await JSON.parse(
+          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+        )
+      );
+    }
+  }, []);
+
+  useEffect(async () => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+      } else {
+        navigate("/setAvatar");
+      }
+    }
+  }, [currentUser]);
+
+
   return (
-    <div>Chat</div>
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} />
+      </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #112D32;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+    @media screen and (min-width: 360px) and (max-width: 480px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
 export default Chat
